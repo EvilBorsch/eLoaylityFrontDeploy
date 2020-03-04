7759df5ff99cd8a71799615393e6b2e50932885b
dmitrijgulacenkov <pingvin1959@yandex.ru>
2020-03-04 16:25:22 +0300

    Новая версия сервера для роутера

--- server.js
+++ server.js
@@ -2,26 +2,28 @@ const http = require('http');
 const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-  let { url } = req;
-  if (/\/$/.test(url)) {
-    url = `${url}index.html`;
-  }
+    let {url} = req;
+    if (/\/$/.test(url)) {
+        url = `${url}index.html`;
+    }
+    let body;
+    try {
+        body = fs.readFileSync(`./public${url}`);
+    } catch (e) {
+        let splitedUrl = url.split('/')
+        let lastPart = splitedUrl[splitedUrl.length - 1]
+        if (lastPart === 'bundle.js') {
+            body = fs.readFileSync('./public/bundle.js')
+        } else {
+            body = fs.readFileSync(`./public/index.html`);
+        }
+    }
+    console.log(url);
 
-  let body;
-  try {
-    body = fs.readFileSync(`./public${url}`);
-  } catch (e) {
-    console.log(e);
-    res.statusCode = 404;
-    res.write('404')
+    res.write(body);
+
     res.end();
-    return;
-  }
-  console.log(url);
-
-  res.write(body);
-
-  res.end();
 });
 
 server.listen(3000);
+console.log('started server localhost:3000')
